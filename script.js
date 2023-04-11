6c5d7b9d8d28a44b8c931b2428eb34da8a56c61e
Philippe <philippe.idalgoprestes@gmail.com>
2023-04-11 17:33:59 -0300

    v1: layout e funções de display lateral

---
+++ script.js
@@ -0,0 +1,9 @@
+function open_side(){
+    document.getElementById("side").style.display = "flex";
+    document.getElementById("blur").style.display = "flex";
+}
+
+function close_side(){
+    document.getElementById("side").style.display = "none";
+    document.getElementById("blur").style.display = "none";
+}
