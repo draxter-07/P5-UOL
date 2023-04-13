08c9899bc46327af657853144d8b0bd0adcc2329
Philippe <philippe.idalgoprestes@gmail.com>
2023-04-13 14:29:24 -0300

    v5 apenas falta aperfeiçoamento do layout e aplicação de funções de layout

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 let tokenApi = 'Ig05OI8F18Lp90ZDISfjWMt8';
 let nome_user = "";
-let to = "oi";
+let to = "Todos";
 
 function open_side(){
     document.getElementById("side").style.display = "flex";
@@ -80,12 +80,27 @@ function atualizar_participantes(){
     b.then(encontrado);
     function encontrado(resposta){
         console.log("Encontrei os participantes");
-        let st = "";
+        document.getElementById("side_part").innerHTML = '';
+        let e = document.createElement('span');
+        e.innerHTML = "Escolha o destinatário";
+        e.classList.add("titulo-side");
+        document.getElementById("side_part").appendChild(e);
+        let q = document.createElement('div');
+        let w = document.createElement('button');
+        w.innerHTML = "Todos";
+        w.classList.add("participante");
+        q.appendChild(w);
+        q.classList.add('destinatario');
+        document.getElementById("side_part").appendChild(q);
         for (let i = 0; i < resposta.data.length; i++){
-            st = st + "<span>" + resposta.data[i].name + "</span><br>";
+            let f = document.createElement('div');
+            let g = document.createElement('button');
+            g.innerHTML = resposta.data[i].name;
+            g.classList.add('participante');
+            f.appendChild(g);
+            f.classList.add('destinatario');
+            document.getElementById("side_part").appendChild(f);
         }
-        console.log("PArticipantes: " + st);
-        document.getElementById("side").innerHTML = st;
     }
     b.catch(nao_encontrado);
     function nao_encontrado(){
@@ -99,12 +114,23 @@ function open_message(){
     a.then(recebi);
     function recebi(resposta){
         console.log("Encontrei as mensagens");
-        let st = "";
+        document.getElementById('messages').innerHTML = '';
         for (let i = 0; i < resposta.data.length; i++){
-            st = st + "<span>" + resposta.data[i].text + "</span><br>";
-        }
-        console.log("Mesagnes: " + st);
-        document.getElementById("messages").innerHTML = st;
+            let j = resposta.data.length - i - 1;
+            let f = document.createElement('div');
+            let g = document.createElement('span');
+            if (resposta.data[j].type == 'status'){
+                g.innerHTML = '<span style="color:rgb(0, 0, 0);font-weight:bold">' + resposta.data[j].from + '</span> ' + resposta.data[j].text;
+                f.classList.add('status');
+                f.appendChild(g);
+            }
+            else if (resposta.data[j].type == 'message'){
+                g.innerHTML = '<span style="color:rgb(200, 200, 200)">' + resposta.data[j].time + '</span> <span style="color:rgb(0, 0, 0);font-weight:bold">' + resposta.data[j].from + '</span> para <span style="color:rgb(0, 0, 0);font-weight:bold">' + resposta.data[j].to + '</span>: ' + resposta.data[j].text;
+                f.classList.add('message');
+                f.appendChild(g);
+            }
+            document.getElementById("messages").appendChild(f);
+        };
     }
     a.catch(nao_recebi)
     function nao_recebi(resposta){
